a0db6e6ae860f17fc974e95b73ae36d0cd5f62d9
likewind2021 <2996814910@qq.com>
2022-01-27 10:38:12 +0800

    feat: 配置邮件服务NodeMailer

--- src/config/MailConfig.js
+++ src/config/MailConfig.js
@@ -18,13 +18,78 @@ async function main () {
     tls: { rejectUnauthorized: false }
   })
 
+  const sendInfo = {
+    code: 'august',
+    expire: '2022-01-28',
+    email: '2996814910@qq.com',
+    user: 'Jackson Yee'
+  }
+
+  const url = 'https://v3.bootcss.com/'
+
   // send mail with defined transport object
   const info = await transporter.sendMail({
     from: '"认证邮件" <2996814910@qq.com>', // sender address
-    to: '2996814910@qq.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>' // html body
+    to: sendInfo.email, // list of receivers
+    subject: sendInfo.user !== '' ? `你好开发者，${sendInfo.user}! 账号注册码` : '账号注册码', // Subject line
+    text: `您正在注册账号，邀请码是${sendInfo.code}，邀请码的过期时间:${sendInfo.expire}`, // plain text body
+    html: `
+    <div
+      style="
+        border: 1px solid #dcdcdc;
+        color: #676767;
+        width: 600px;
+        margin: 0 auto;
+        padding-bottom: 50px;
+        position: relative;
+      "
+    >
+      <div
+        style="
+          height: 60px;
+          background: #393d49;
+          line-height: 60px;
+          color: #58a36f;
+          font-size: 18pax;
+          padding-left: 10px;
+        "
+      >
+        欢迎来到社区
+      </div>
+      <div style="padding: 25px">
+        <div>
+          您好，${sendInfo.user}同学，重置链接有效时间30分钟，请在${sendInfo.expire}之前重置您的密码:
+        </div>
+        <a
+          href="${url}"
+          style="
+            padding: 10px 20px;
+            color: #fff;
+            background: #009e94;
+            display: inline-block;
+            margin: 15px 0;
+          "
+          >立即重置密码</a
+        >
+        <div style="padding: 5px; background: #f2f2f2">
+          如果该邮件不是由你本人操作，请勿进行激活！否则你的邮箱将会被他人绑定。
+        </div>
+      </div>
+      <div
+        style="
+          background: #fafafa;
+          color: #b4b4b4;
+          text-align: center;
+          line-height: 45px;
+          height: 45px;
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+        "
+      >系统邮件，请勿直接回复</div>
+    </div>
+    ` // html body
   })
 
   console.log('Message sent: %s', info.messageId)
