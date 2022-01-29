f0084c93f5b39980084ea9d8e4fe40819795ec30
likewind2021 <2996814910@qq.com>
2022-01-29 15:26:38 +0800

    feat: 接口调用

--- src/api/login.js
+++ src/api/login.js
@@ -1,31 +1,22 @@
-import axios from 'axios'
+import axios from '../utils/request'
 
-const getCode = async () => {
-  let result = ''
-  try {
-    result = await axios.get('/getCaptcha')
-    if (result.status === 200) {
-      return result.data
+const getCode = (sid) => {
+  console.log(sid)
+  // axios.request({
+  //    method: 'get',
+  //    url: '/getCaptcha'
+  // })
+  return axios.get('/getCaptcha', {
+    params: {
+      sid: sid
     }
-  } catch (e) {
-    console.log(e)
-  }
-  return result
+  })
 }
 
-const forget = async (option) => { // option 传递过来的一些用户数据
-  let result = ''
-  try {
-    result = await axios.post('api/forget', {
-      ...option
-    })
-    if (result.status === 200) {
-      return result.data
-    }
-  } catch (e) {
-    console.log(e)
-  }
-  return result
+const forget = (option) => { // option 传递过来的一些用户数据
+  return axios.post('/forget', {
+    ...option
+  })
 }
 
 export {
