d8166aed456815b0170bec117fbb6ef6b45813f5
likewind2021 <2996814910@qq.com>
2022-01-29 15:23:06 +0800

    feat：完成邮件服务接口

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 const combineRoutes = require('koa-combine-routers')
 
-// const publicRouter = require('./publicRouter')
+const publicRouter = require('./publicRouter')
 const loginRouter = require('./loginRouter')
 
-module.exports = combineRoutes(loginRouter)
+module.exports = combineRoutes(loginRouter, publicRouter)
 // export default combineRoutes(publicRouter, loginRouter) // 整合所有路由
