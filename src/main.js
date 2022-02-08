b28d10ef955dc826ef6c44c65bc8acd5553f1189
likewind2021 <2996814910@qq.com>
2022-02-08 09:29:11 +0800

    feat(输入校验): Veevalidate结合vue-i18n

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,13 +11,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.css'
 
 // veevalidate
-// import './util/veevalidate'
-import './util/veevalidate-i18n'
+// import './utils/veevalidate'
+import './utils/veevalidate-i18n'
 
 import './style/reset.css'
 Vue.config.productionTip = false
-
-axios.defaults.baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:3000' : 'http://localhost:8080/'
 
 new Vue({
   router,
