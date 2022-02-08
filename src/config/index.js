35663e928f31195b4321462cd25760f4e16c3a5d
likewind2021 <2996814910@qq.com>
2022-02-08 09:25:35 +0800

    feat(配置文件): 配置JWT_SECRET

--- src/config/index.js
+++ src/config/index.js
@@ -7,8 +7,8 @@ const REDIS = {
 
 const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
 
+module.exports = { DB_URL, REDIS, JWT_SECRET }
+
 // export default {
 //   DB_URL, REDIS, JWT_SECRET
 // }
-
-module.exports = { DB_URL, REDIS, JWT_SECRET }
