09f10f3a2606f1b54c400fe5368b377546098cbd
likewind2021 <2996814910@qq.com>
2022-02-09 18:08:22 +0800

    feat: 处理错误信息

---
+++ src/components/modules/alert/index.js
@@ -0,0 +1,34 @@
+import AlertComponent from './Alert.vue'
+
+const Alert = {}
+
+Alert.install = (Vue) => {
+  // 把 Alert 组件绑定到 Vue 的全局实例上
+  const AlertConstructor = Vue.extend(AlertComponent)
+  const instance = new AlertConstructor()
+  instance.$mount(document.createElement('div'))
+  document.body.appendChild(instance.$el)
+
+  // 4. 添加实例方法
+  Vue.prototype.$alert = (msg) => {
+    // 逻辑...
+    instance.type = 'alert'
+    instance.msg = msg
+    instance.isShow = true
+  }
+
+  Vue.prototype.$confirm = (msg, success, cancel) => {
+    // 逻辑...
+    instance.type = 'confirm'
+    instance.msg = msg
+    instance.isShow = true
+    if (typeof success !== 'undefined') {
+      instance.success = success
+    }
+    if (typeof cancel !== 'undefined') {
+      instance.cancel = cancel
+    }
+  }
+}
+
+export default Alert
