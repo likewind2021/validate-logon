5a6b465f0ee0d44d4f41b35e289a362f55d7ac5c
likewind2021 <2996814910@qq.com>
2022-02-10 17:40:42 +0800

    feat(login): 登录接口开发

--- src/api/login.js
+++ src/api/login.js
@@ -1,24 +1,51 @@
 import axios from '../utils/request'
 
+/**
+ * 获取验证码接口
+ * @param {*} sid 唯一标识
+ */
 const getCode = (sid) => {
-  console.log(sid)
   // axios.request({
   //    method: 'get',
   //    url: '/getCaptcha'
   // })
-  return axios.get('/getCaptcha', {
+  return axios.get('/public/getCaptcha', {
     params: {
       sid: sid
     }
   })
 }
 
+/**
+ * 找回密码接口
+ * @param {*} option 用户信息（邮箱、验证码）
+ */
 const forget = (option) => { // option 传递过来的一些用户数据
-  return axios.post('/forget', {
+  return axios.post('/login/forget', {
     ...option
   })
 }
 
+/**
+ * 登录接口
+ * @param {*} loginInfo 用户登录信息
+ */
+const login = (loginInfo) => {
+  return axios.post('/login/login', {
+    ...loginInfo
+  })
+}
+
+/**
+ * 注册接口
+ * @param {*} regInfo 用户注册信息
+ */
+const reg = (regInfo) => {
+  return axios.post('/login/reg', {
+    ...regInfo
+  })
+}
+
 export {
-  getCode, forget
+  getCode, forget, login, reg
 }
