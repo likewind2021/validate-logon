e908099a0842d6f04add8dacf5bf766aacf500f8
likewind2021 <2996814910@qq.com>
2022-02-10 17:45:55 +0800

    feat(getCaptcha): 获取图形验证码接口开发

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -1,21 +1,25 @@
-// const setValue = require('../config/RedisConfig')
+const { setValue } = require('../config/RedisConfig')
 const svgCaptcha = require('svg-captcha')
 
 class PublicController {
   // eslint-disable-next-line no-useless-constructor
   constructor () { }
   async getCaptcha (ctx) {
-    // const body = ctx.request.query
+    const body = ctx.request.query
     const newCaptcha = svgCaptcha.create({
       size: 4,
       ignoreChars: '0o1il',
       color: true,
       noise: Math.floor(Math.random() * 5),
-      width: 150,
-      height: 28
+      width: 120,
+      height: 38
     })
     // 保存图片验证码数据，设置超时时间，单位：s
-    // setValue(body.sid, newCaptcha.text, 10 * 60)
+    try {
+      setValue(body.sid, newCaptcha.text, 10 * 60)
+    } catch (error) {
+      console.log(error)
+    }
     // getValue(body.sid).then((res) => {
     //   console.log(res)
     // })
