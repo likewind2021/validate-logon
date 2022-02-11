c7588f41aad9bdf15357cad6d4c825afa82117a2
likewind2021 <2996814910@qq.com>
2022-02-11 14:18:12 +0800

    feat: 服务端 koa 统一错误处理

--- src/common/errorHandle.js
+++ src/common/errorHandle.js
@@ -4,10 +4,15 @@ module.exports = (ctx, next) => {
       ctx.status = 401
       ctx.body = {
         code: 401,
-        message: 'Protected resource, use Authorization header to get access\n'
+        msg: 'Protected resource, use Authorization header to get access\n'
       }
     } else {
-      throw err
+      ctx.status = err.status || 500
+      ctx.body = Object.assign({
+        code: 500,
+        msg: err.message
+      }, process.env.NODE_ENV === 'development' ? { stack: err.stack } : {})
+      // process.env.NODE_ENV 在 webpack打包过程中生效
     }
   })
 }
