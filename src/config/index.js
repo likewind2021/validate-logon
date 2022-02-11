cfd88edc1f420d9d6fe2f2d1634329822ddf7f88
likewind2021 <2996814910@qq.com>
2022-02-11 11:18:42 +0800

    fix: 修复 DB_URL, REDIS 配置问题

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,17 @@
-const DB_URL = 'mongodb://test:123456@47.105'
+const DB_URL = 'mongodb://127.0.0.1'
 const REDIS = {
-  host: '47.105.212.161',
-  port: 15001,
-  password: 123456
+  host: '127.0.0.1',
+  port: 6379,
+  password: 'requirepass'
 }
 
 const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
-
 module.exports = { DB_URL, REDIS, JWT_SECRET }
 
 // export default {
 //   DB_URL, REDIS, JWT_SECRET
 // }
+
+// const baseUrl = process.env.NODE_ENV === 'production' ? 'http://www.luckmiss.com' : 'http://localhost:3000'
+// const uploadPath = process.env.NODE_ENV === 'production' ? '/app/public' : path.join(path.resolve(__dirname), '../../public')
+// export default { DB_URL, REDIS, JWT_SECRET, baseUrl, uploadPath }
