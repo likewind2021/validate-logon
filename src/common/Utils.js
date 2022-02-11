356b61500c86d14e8664766fe6cd903c3627baff
likewind2021 <2996814910@qq.com>
2022-02-11 11:05:03 +0800

    fix(图形验证码校验): 修复toLowerCase()使用错误

--- src/common/Utils.js
+++ src/common/Utils.js
@@ -1,10 +1,10 @@
-const getValue = require('../config/RedisConfig')
+const { getValue } = require('../config/RedisConfig')
 
 const checkCode = async (key, value) => {
   const redisData = await getValue(key)
   if (redisData != null) {
     // 注意使大小写不敏感
-    if (redisData.toLowerCase === value.toLowerCase) {
+    if (redisData.toLowerCase() === value.toLowerCase()) {
       return true
     } else {
       return false
