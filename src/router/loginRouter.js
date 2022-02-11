71c797bd32ed8cec25414ce43e784bf935cc0555
likewind2021 <2996814910@qq.com>
2022-02-11 14:12:07 +0800

    feat: 添加注册业务路径

--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -7,6 +7,7 @@ router.prefix('/login')
 router.post('/forget', loginController.forget) // 管理业务对应的路径
 // router.get('/test', loginController.test)
 router.post('/login', loginController.login)
+router.post('/reg', loginController.reg)
 
 module.exports = router
 // export default router
